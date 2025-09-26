b470a25751ef3a31c96baf30c64e7a424646e124
DESKTOP-2IK9HPO\IreneOD <ireneod20@gmail.com>
2025-09-26 12:40:07 +0200

    feat: subida actividad 2.2

---
+++ Actividades/Act2_2/js/myJS1.js
@@ -0,0 +1,4 @@
+
+document.writeln('<i class="fa-regular fa-file"></i>');
+console.log("Hola desde myJS1.js");
+window.alert("Hola desde myJS1.js");
---
+++ Actividades/Act2_2/js/myJS2.js
@@ -0,0 +1,3 @@
+document.writeln("<h3>Hola desde myJS2.js</h3>");
+console.log("Hola desde myJS2.js");
+window.alert("Hola desde myJS2.js");
